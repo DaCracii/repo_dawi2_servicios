69b27f3fb8b6db664fe0535ee88099951514d1f5
DaCracii <ellopz887@gmail.com>
2021-05-23 15:27:42 -0500

    1 commit

---
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -0,0 +1,45 @@
+package com.empresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.entity.Alumno;
+import com.empresa.service.AlumnoService;
+
+@RestController
+@RequestMapping("/rest/alumno")
+public class AlumnoController { //RestController para consumir servicios, ya no una aplicacion web
+
+	@Autowired
+	private AlumnoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Alumno>> listaAlumno(){
+		System.out.println(">>>> lista ");
+		List<Alumno> lstAlumno= service.listaAlumno();
+		
+		return ResponseEntity.ok(lstAlumno); //para convertir nuestro Arraylist en Json
+	}
+	
+	@PostMapping
+	public ResponseEntity<Alumno> registra(@RequestBody Alumno obj){ //indica que Alumno va a venir en formato Json
+		System.out.println(">>>> Registro ");
+		Alumno objSalida = service.insertaActualizaAlumno(obj);
+		if(objSalida != null) {
+			return ResponseEntity.ok(objSalida);
+		}else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+}
+
+
+
